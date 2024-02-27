10fce86ab11592a2c7e66578dcaec1e39509debe
liujiaodaliao <mengmeijia@126.com>
2024-02-28 00:55:43 +0100

    Fix carousel logic

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,18 +7,37 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+  // const byDateDesc = data?.focus.sort((evtA, evtB) =>
+  //   new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  // );
+  // const nextCard = () => {
+  //   setTimeout(
+  //     () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+  //     5000
+  //   );
+  // };
+  // useEffect(() => {
+  //   nextCard();
+  // });
+  
+  
+  // 修改日期渲染逻辑
+  const byDateDesc = data?.focus ? [...data.focus].sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+  ) : [];
+
+  // 修改循环播放逻辑
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0)), // 如果到达最后一张，则循环回第一张
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
-  });
+  }, [index]); // 添加 index 作为 useEffect 依赖项，确保每次 index 变化时重新计时
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
