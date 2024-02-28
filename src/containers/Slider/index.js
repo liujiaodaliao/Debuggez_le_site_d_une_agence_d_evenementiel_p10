bc50f17c1ad56a26953ec1280bf6f2949aec0253
liujiaodaliao <mengmeijia@126.com>
2024-02-28 01:56:14 +0100

    fix slider index input

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -28,24 +28,27 @@ const Slider = () => {
 
   // 修改循环播放逻辑
   const nextCard = () => {
-    setTimeout(
-      () => setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0)), // 如果到达最后一张，则循环回第一张
-      5000
-    );
+    // setTimeout(
+    //   () => setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0)), // 如果到达最后一张，则循环回第一张
+    //   5000
+    // );
+    setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
   };
 
   useEffect(() => {
-    nextCard();
-  }, [index]); // 添加 index 作为 useEffect 依赖项，确保每次 index 变化时重新计时
+    const timer = setTimeout(nextCard, 5000);
+    return () => clearTimeout(timer); // 清除定时器以避免内存泄漏
+  }, [index, byDateDesc]);
 
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
+      {byDateDesc?.map((event, eventIdx) => (  // 将 idx 改为 eventIdx
         <>
           <div
-            key={event.title}
+            // key={event.title}
+            key={event.id} 
             className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+              index ===  eventIdx ? "display" : "hide"
             }`}
           >
             <img src={event.cover} alt="forum" />
@@ -59,12 +62,15 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((_, idx) => (
                 <input
-                  key={`${event.id}`}
+                  // key={`${event.id}`}
+                  key={event.id}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  // checked={idx === radioIdx}
+                  checked={index === idx}
+                  onChange={() => setIndex(idx)} // 点击时切换到对应图片
                 />
               ))}
             </div>
